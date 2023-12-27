43d422c2f3a6917d25f25e25186a5333ffa03031
Daun <iam@daun.io>
2023-12-27 13:04:20 +0900

    finetune

--- apps/web/app/character-card.tsx
+++ apps/web/app/character-card.tsx
@@ -16,9 +16,12 @@ const CharacterCard = (props: {
 }) => {
   console.log("props::", props);
   return (
-    <AspectRatio ratio={1 / 1.75}>
-      <Link href={`/character/${props?.id}`} className="group">
-        <Card className="rounded-lg p-2 w-[calc(100%-1rem)] h-[calc(100%-1rem)] flex items-end shadow hover:shadow-xl duration-200">
+    <AspectRatio
+      ratio={1 / 1.75}
+      className="group w-full h-full hover:-translate-y-1 duration-200 shadow hover:shadow-lg place-content-center rounded-lg"
+    >
+      <Link href={`/character/${props?.id}`}>
+        <Card className="rounded-lg p-2 w-full h-full flex items-end">
           <CardHeader className="relative w-full p-4">
             <div className="bg-gradient-to-b from-transparent to-black/75 absolute -left-2 -bottom-2 w-[calc(100%+16px)] h-[calc(100%+2rem)] rounded-b-lg" />
             <CardTitle className="text-white text-xl select-none line-clamp-1 z-[1] group-hover:opacity-80 duration-200">
@@ -35,7 +38,7 @@ const CharacterCard = (props: {
               width={300}
               height={525}
               quality={75}
-              className="object-cover absolute w-[calc(100%-16px)] h-[calc(100%-16px)] rounded-lg left-0 top-0 pointer-events-none"
+              className="object-cover absolute w-full h-full rounded-lg left-0 top-0 pointer-events-none"
             />
           )}
         </Card>
