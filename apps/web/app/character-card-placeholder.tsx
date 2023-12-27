e90f11f0a88c784c752a96473354bc27600ebf65
Daun <iam@daun.io>
2023-12-27 13:16:17 +0900

    simplify placeholder card

--- apps/web/app/character-card-placeholder.tsx
+++ apps/web/app/character-card-placeholder.tsx
@@ -5,20 +5,25 @@ import {
   CardTitle,
 } from "@repo/ui/src/components";
 import { AspectRatio } from "@repo/ui/src/components/aspect-ratio";
+import Image from "next/image";
 
-const CharacterCardPlaceholder = () => {
+const CharacterCardPlaceholder = (props: {}) => {
   return (
-    <AspectRatio ratio={1 / 1.75}>
-      <Card className="rounded-lg p-2 w-[calc(100%-1rem)] h-[calc(100%-1rem)] shadow-lg flex items-end ring ring-black/10">
+    <AspectRatio
+      ratio={1 / 1.75}
+      className="group w-full h-full hover:-translate-y-1 duration-200 shadow hover:shadow-lg place-content-center rounded-lg"
+    >
+      <Card className="rounded-lg p-2 w-full h-full flex items-end">
         <CardHeader className="relative w-full p-4">
           <div className="bg-gradient-to-b from-transparent to-black/75 absolute -left-2 -bottom-2 w-[calc(100%+16px)] h-[calc(100%+2rem)] rounded-b-lg" />
-          <CardTitle className="text-white select-none line-clamp-1 z-[1] text-xl">
+          <CardTitle className="bg-gray-300 rounded-md text-gray-300 text-xl select-none line-clamp-1 z-[1] group-hover:opacity-80 duration-200">
             Loading...
           </CardTitle>
-          <CardDescription className="text-white select-none line-clamp-3 z-[1]">
+          <CardDescription className="bg-gray-300 text-gray-300 rounded-md select-none line-clamp-3 z-[1] group-hover:opacity-80 duration-200">
             Loading...
           </CardDescription>
         </CardHeader>
+        <div className="object-cover absolute w-full h-full rounded-lg left-0 top-0 pointer-events-none bg-gray-100" />
       </Card>
     </AspectRatio>
   );
