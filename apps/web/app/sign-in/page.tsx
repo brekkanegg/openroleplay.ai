f2812f009118e4f57e969719d8495dc95125fa4b
Daun <iam@daun.io>
2023-12-31 21:52:14 +0900

    fix signin page layout

--- apps/web/app/sign-in/page.tsx
+++ apps/web/app/sign-in/page.tsx
@@ -2,5 +2,11 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="w-full h-[100vh] max-w-screen-xl flex flex-col justify-self-start">
+      <div className="w-full h-full items-start justify-center flex py-32">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
