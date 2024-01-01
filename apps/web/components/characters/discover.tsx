841dc10f2d47788a8e1bc4f4fdd95a0df3624730
Daun <iam@daun.io>
2024-01-02 07:09:16 +0900

    fix placeholders

--- apps/web/components/characters/discover.tsx
+++ apps/web/components/characters/discover.tsx
@@ -25,7 +25,7 @@ const Discover = () => {
   return (
     <>
       <div className="px-4 2xl:px-0 flex flex-col sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full gap-4">
-        {characters
+        {characters?.length > 0
           ? characters.map(
               (character, index) =>
                 character.name && (
